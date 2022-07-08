289851386ae24ec651ea1061f6e838d6a06fb3fa
Hernán Astudillo <hernan.astudillo.r@gmail.com>
2022-07-07 21:13:56 -0400

    feat() postgres on heroku

--- database.py
+++ database.py
@@ -1,11 +1,22 @@
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "postgresql://myuser:password@localhost/fast_api"
+print('##################################################################################')
+print(os.environ)
+print('##################################################################################')
 
+print('##################################################################################')
+print(os.environ['DATABASE_URL'])
+print('##################################################################################')
+
+DATABASE_URL = os.environ['DATABASE_URL']
 engine = _sql.create_engine(DATABASE_URL)
 
-SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = _orm.sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
 
 Base = _declarative.declarative_base()
